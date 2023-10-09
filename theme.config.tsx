bc7df959934d21e1dc68affaeb5eec9daacf6e09
b5h22 <b5h2xx@gmail.com>
2023-10-09 12:29:05 -0400

    first commit

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>My Project</span>,
+  project: {
+    link: 'https://github.com/b5h22/dev-notes',
+  },
+  // chat: {
+  //   link: 'https://discord.com',
+  // },
+  docsRepositoryBase: 'https://github.com/b5h22/dev-notes/pages',
+  footer: {
+    text: 'Made by b5h22',
+  },
+}
+
+export default config
