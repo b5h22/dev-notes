c69b348cf060a02f35c21da335966ff33c7fa285
b5h22 <b5h2xx@gmail.com>
2023-10-17 14:52:02 -0400

    fix: invalid file root

--- config/constants.ts
+++ config/constants.ts
@@ -1,5 +1,5 @@
 export const description =
-  'The notes from my journey in learning software development.'
+  'The notes from my journey in learning software development.';
 
-const isProduction = process.env.NODE_ENV === 'production'
-export const assetPrefix = isProduction ? '/dev-notes' : ''
+const isProduction = process.env.NODE_ENV === 'production';
+export const assetPrefix = isProduction ? '/dev-notes' : '';
